487779f7975471b30c411e7e6b7ce28af6b53274
Manish Bishwokarma <122954766+sailenceresw@users.noreply.github.com>
2026-08-16 06:16:33 +0100

    fix: replace debug prints with log in EditorController

--- lib/src/controllers/editor_controller.dart
+++ lib/src/controllers/editor_controller.dart
@@ -36,13 +36,12 @@ class EditorController extends GetxController {
       selectedVideoPaths
         ..clear()
         ..addAll(result.paths.map((path) => path!).toList());
-      print(selectedVideoPaths);
-      // Use the filePaths list to do something with the selected video files
-      // Build the FFmpeg command to concatenate the videos
+      log('Picked paths: $selectedVideoPaths');
       if (selectedVideoPaths.isNotEmpty) {
         if (selectedVideoPaths.length == 1) {
           Get.back();
           editingVideoFile = File(selectedVideoPaths[0]);
+          currentPlayablePath = editingVideoFile!.path;
           update();
           Get.to(() => CustomVideoEditor(file: editingVideoFile));
         } else {
@@ -52,7 +51,6 @@ class EditorController extends GetxController {
 
       log("Picked video files paths: $selectedVideoPaths");
     } else {
-      // User canceled the picker
       log("User canceled the picker");
     }
   }
@@ -66,6 +64,7 @@ class EditorController extends GetxController {
     );
     if (captured != null) {
       editingVideoFile = File(captured.path);
+      currentPlayablePath = editingVideoFile!.path;
       update();
       Get.to(() => CustomVideoEditor(file: editingVideoFile));
     } else {
@@ -81,7 +80,7 @@ class EditorController extends GetxController {
     }
 
     final request = await Permission.manageExternalStorage.request();
-    log("Permmission status :$request");
+    log("Permission status :$request");
     if (request == PermissionStatus.granted) {
       return true;
     }
@@ -93,8 +92,6 @@ class EditorController extends GetxController {
     update();
     String basePath = await getOutputDirectoryPath();
     String outputPath = "concat.mp4";
-    // String command =
-    //     '-y -i "concat:${selectedVideoPaths.join('|')}" -c copy $outputFilePath';
     List<String> tempFiles = [];
     for (int i = 0; i < selectedVideoPaths.length; i++) {
       String tempFilePath = "$basePath${i}_temp.ts";
@@ -107,26 +104,23 @@ class EditorController extends GetxController {
             FFmpegKitConfig.sessionStateToString(await session.getState());
         final output = await session.getOutput();
         if (ReturnCode.isSuccess(returnCode)) {
-          print("Successfully converted to ts file");
+          log("Successfully converted to ts file");
         } else if (ReturnCode.isCancel(returnCode)) {
-          print("Cancel converted to ts file");
+          log("Cancel converted to ts file");
         } else {
-          print("Error converted to ts file");
+          log("Error converted to ts file");
         }
         log(state);
         log(output.toString());
         log(returnCode.toString());
-        log("++++++++++++++++++++++++++++++++++++++++++++++++++++++++");
       });
     }
 
     String concatCommand = "concat:${tempFiles.join('|')}";
-    // await FFmpegKit.execute(
-    //     "-i \"$concatCommand\" -c copy -bsf:a aac_adtstoasc $outputFilePath");
-    print(concatCommand);
+    log(concatCommand);
     String command =
         '-y -i "$concatCommand" -c copy -bsf:a aac_adtstoasc ${basePath + outputPath}';
-    print(command);
+    log(command);
 
     FFmpegKit.execute(command).then((session) async {
       final returnCode = await session.getReturnCode();
@@ -134,27 +128,15 @@ class EditorController extends GetxController {
           FFmpegKitConfig.sessionStateToString(await session.getState());
       final output = await session.getOutput();
       if (ReturnCode.isSuccess(returnCode)) {
-        print("Succesfully merged");
+        log("Successfully merged");
         Get.back();
         isCombining = false;
-        update();
-        // Get.dialog(
-        //   Dialog(
-        //     child: VideoPlayer(
-        //       VideoPlayerController.file(
-        //         File(basePath + outputPath),
-        //       )
-        //         ..initialize()
-        //         ..play(),
-        //     ),
-        //   ),
-        // );
         editingVideoFile = File(basePath + outputPath);
+        currentPlayablePath = editingVideoFile!.path;
         update();
         Get.to(() => CustomVideoEditor(file: editingVideoFile));
       } else if (ReturnCode.isCancel(returnCode)) {
         log("Cancelled merged");
-
         Get.back();
         isCombining = false;
         update();
@@ -163,7 +145,6 @@ class EditorController extends GetxController {
         Get.snackbar("Error", "Something messed up:$returnCode ");
       } else {
         log("Error merged : $returnCode");
-
         Get.back();
         isCombining = false;
         update();
@@ -181,29 +162,6 @@ class EditorController extends GetxController {
     update();
     Get.back();
   }
-
-  //filter option
-  // late final VideoPreviewController controller;
-  // late final GPUVideoPreviewParams previewParams;
-  // bool previewParamsReady = false;
-  // late final GPUFilterConfiguration configuration;
-
-  // brightness(double brightness) {
-  //   GPUBrightnessConfiguration().brightness = brightness;
-  //   update();
-  // }
-
-  // filterPrepare() {
-  //   _prepare().whenComplete(() {
-  //     update();
-  //   });
-  // }
-
-  // Future<void> _prepare() async {
-  //   await widget.configuration.prepare();
-  //   previewParams = await GPUVideoPreviewParams.create(widget.configuration);
-  //   previewParamsReady = true;
-  // }
 
   Color selectedColor = Colors.black;
 
@@ -226,6 +184,7 @@ class EditorController extends GetxController {
 
   changeVideoPlayablePath(String path) async {
     currentPlayablePath = path;
+    editingVideoFile = File(path);
     update();
   }
 
