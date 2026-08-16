e35cfa2d4e7bf5c577b8e9e8c22060361fa94932
Manish Bishwokarma <122954766+sailenceresw@users.noreply.github.com>
2026-08-16 06:02:26 +0100

    fix: make CustomTextFormField fields final to satisfy must_be_immutable (#38)

--- lib/src/widgets/custom_textfield.dart
+++ lib/src/widgets/custom_textfield.dart
@@ -1,54 +1,54 @@
 import 'package:flutter/material.dart';
 
 class CustomTextFormField extends StatelessWidget {
-  TextEditingController? controller;
-  dynamic formKey;
-  String? hintText;
-  TextInputType textInputType;
-  String? labelText;
-  Widget? suffix;
-  bool? isEnabled;
-  bool readOnly;
-  bool obscureText;
+  final TextEditingController? controller;
+  final dynamic formKey;
+  final String? hintText;
+  final TextInputType textInputType;
+  final String? labelText;
+  final Widget? suffix;
+  final bool? isEnabled;
+  final bool readOnly;
+  final bool obscureText;
 
   final Function? validator;
-  bool onlyText;
-  bool onlyNumber;
-  int? maxLine;
-  int? minLine;
-  int? maxLength;
-  String? prefixText;
-  bool? filled;
-  Color? fillColor;
-  IconData? prefixIcon;
-  Function()? onTap;
-  Function? onChanged;
-  Function? onFieldSubmitted;
-  String? initialValue;
-  bool? isSearch;
-  bool? autoFocus;
-  AutovalidateMode? autovalidateMode;
-  List<String> autoFillHint;
-  bool searchString;
-  bool fullNameString;
-  bool allowMultipleSpace;
-  bool? showBorder;
-  TextInputAction? textInputAction;
-  double borderRadius;
-  double? hintTextSize;
-  FontWeight? hintTextWeight;
-  double? enteredTextSize;
-  FontWeight? enteredTextWeight;
-  TextAlign? textAlignment;
-  bool? notFromFormType = false;
-  bool? allowDouble;
-  bool? onlyPhoneNumber;
-  dynamic prefixIconSize;
-  bool? isPrefixText;
-  Widget? prefix;
-  Widget? suffixInside;
+  final bool onlyText;
+  final bool onlyNumber;
+  final int? maxLine;
+  final int? minLine;
+  final int? maxLength;
+  final String? prefixText;
+  final bool? filled;
+  final Color? fillColor;
+  final IconData? prefixIcon;
+  final Function()? onTap;
+  final Function? onChanged;
+  final Function? onFieldSubmitted;
+  final String? initialValue;
+  final bool? isSearch;
+  final bool? autoFocus;
+  final AutovalidateMode? autovalidateMode;
+  final List<String> autoFillHint;
+  final bool searchString;
+  final bool fullNameString;
+  final bool allowMultipleSpace;
+  final bool? showBorder;
+  final TextInputAction? textInputAction;
+  final double borderRadius;
+  final double? hintTextSize;
+  final FontWeight? hintTextWeight;
+  final double? enteredTextSize;
+  final FontWeight? enteredTextWeight;
+  final TextAlign? textAlignment;
+  final bool? notFromFormType;
+  final bool? allowDouble;
+  final bool? onlyPhoneNumber;
+  final dynamic prefixIconSize;
+  final bool? isPrefixText;
+  final Widget? prefix;
+  final Widget? suffixInside;
 
-  CustomTextFormField({
+  const CustomTextFormField({
     Key? key,
     this.formKey,
     this.suffixInside,
@@ -107,8 +107,6 @@ class CustomTextFormField extends StatelessWidget {
       maxLength: maxLength,
       textInputAction: textInputAction ?? TextInputAction.done,
       autofillHints: autoFillHint,
-      // autofocus: autoFocus ?? false,
-
       validator: (value) {
         return validator == null ? null : validator!(value);
       },
@@ -118,7 +116,6 @@ class CustomTextFormField extends StatelessWidget {
         fontFamily: "Quicksand",
         fontWeight: enteredTextWeight ?? FontWeight.w400,
       ),
-
       readOnly: readOnly,
       initialValue: initialValue,
       enabled: isEnabled,
@@ -138,9 +135,6 @@ class CustomTextFormField extends StatelessWidget {
           fontWeight: FontWeight.w500,
         ),
         filled: filled,
-        // contentPadding: notFromFormType == true
-        //     ? EdgeInsets.zero
-        //     : const EdgeInsets.all(8.0),
         labelStyle: const TextStyle(
           fontFamily: "Quicksand",
           color: Colors.grey,
@@ -153,7 +147,6 @@ class CustomTextFormField extends StatelessWidget {
         ),
         hintStyle: TextStyle(
           fontFamily: "Quicksand",
-          // fontSize: 16.0,
           color: Colors.grey,
           fontSize: hintTextSize ?? 15,
           fontWeight: hintTextWeight ?? FontWeight.w400,
